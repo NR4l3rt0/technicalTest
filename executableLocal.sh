38165c3951f3c7266cdd95ea920061086e7868fb
NR Alberto <alberto.ruizm@outlook.com>
2022-01-13 11:59:30 +0100

    set it like it was 1,5 hours ago

--- executableLocal.sh
+++ executableLocal.sh
@@ -4,30 +4,14 @@
 mvn clean package
 
 # Create an image locally
-docker build -t technical-test-docker.jar:v3.0 .
+docker build -t 4lb3rt0/technical-test-docker.jar:v2.0 .
 
 # Create the cluster 
 minikube start
 
 # Create a deployment object with the docker image in the cluster
-#kubectl apply -f deployment.yaml
-kubectl create deployment technical-test-local-hello --image=technical-test-docker.jar:v3.0
-
-# Expose deployment while creating the service
-kubectl expose deployment balanced --type=LoadBalancer --port=8888
-
-kubectl get deployments
-kubectl get service
-
-# Create a deployment object with the docker image in the cluster
-#kubectl apply -f deployment.yaml
-#kubectl create deployment technical-test-local-hello --image=yp
+kubectl apply -f deployment.yaml
 
 # Create a service object that expose the deployement
 kubectl apply -f service.yaml
 
-# Create a service object that expose the deployement
-kubectl apply -f service.yaml
-
-# Returns the default endpoint since there is no redirection
-minikube service technical-test-k8s-service --url
