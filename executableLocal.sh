2a62892b55b6e0862a0c6fcd1c86091f18e5ea86
NR Alberto <alberto.ruizm@outlook.com>
2022-01-13 11:56:28 +0100

    test reset from repo

--- executableLocal.sh
+++ executableLocal.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/bash
 
+# Create the jar file
 mvn clean package
 
-docker build -t 4lb3rt0/technical-test-docker.jar:v2.0 .
+# Create an image locally
+docker build -t technical-test-docker.jar:v3.0 .
 
+# Create the cluster 
 minikube start
 
-kubectl apply -f deployment.yaml
+# Create a deployment object with the docker image in the cluster
+#kubectl apply -f deployment.yaml
+kubectl create deployment technical-test-local-hello --image=technical-test-docker.jar:v3.0
 
+# Expose deployment while creating the service
+kubectl expose deployment balanced --type=LoadBalancer --port=8888
+
+kubectl get deployments
+kubectl get service
+
+# Create a deployment object with the docker image in the cluster
+#kubectl apply -f deployment.yaml
+#kubectl create deployment technical-test-local-hello --image=yp
+
+# Create a service object that expose the deployement
 kubectl apply -f service.yaml
+
+# Create a service object that expose the deployement
+kubectl apply -f service.yaml
+
+# Returns the default endpoint since there is no redirection
+minikube service technical-test-k8s-service --url
